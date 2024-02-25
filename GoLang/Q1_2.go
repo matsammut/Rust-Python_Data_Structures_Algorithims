125323a83fc72af74852909153027ef187d32363
Matteo <matteo.sammut.20@um.edu.mt>
2024-02-25 15:26:17 +0100

    Update Q1_2.go
    Added method to create random arrays

--- GoLang/Q1_2.go
+++ GoLang/Q1_2.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	// "fmt"
+	"math/rand/v2"
+	// "reflect"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	var len_of_A = rand.Int32N(100) + 256
+
+	A := make([]int32, len_of_A)
+	var i int32 = 0
+	for i = 0; i < len_of_A; i++ {
+		A[i] = rand.Int32N(1024)
+	}
+
+	len_of_B := rand.Int32N(100) + 256
+
+	B := make([]int32, len_of_B)
+	for i = 0; i < len_of_B; i++ {
+		B[i] = rand.Int32N(1024)
+	}
+}
+
+func populateArray() {
+	var len_of_A = rand.Int32N(100) + 256
+	A := make([]int32, len_of_A)
+	var i int32 = 0
+	for i = 0; i < len_of_A; i++ {
+		A[i] = rand.Int32N(1024)
+	}
+	return A
 }
